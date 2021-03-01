ba6aada61c0c495463e2dc9305fd88b257da3ab8
Abdelrhman Gad <24657115+Abdelrhmangad@users.noreply.github.com>
2021-03-01 11:53:47 +0200

    Deleting comments

--- js/main.js
+++ js/main.js
@@ -1,31 +1,3 @@
-// var slideIndex = 1;
-// showDivs(slideIndex);
-
-// function plusDivs(n) {
-//   showDivs(slideIndex += n);
-// }
-
-// function currentDiv(n) {
-//   showDivs(slideIndex = n);
-// }
-
-// function showDivs(n) {
-//   var i;
-//   var x = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("demo");
-//   if (n > x.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = x.length}
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";  
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" w3-white", "");
-//   }
-//   x[slideIndex-1].style.display = "block";  
-//   dots[slideIndex-1].className += " w3-white";
-// }
-
-
 const carsouelImage = document.getElementById("carousel-image"),
     dot1 = document.getElementById("dot1"),
     dot2 = document.getElementById("dot2"),
@@ -37,24 +9,6 @@ let images = [
     "images/clothis 2.png",
     "images/clothis 3.png"
 ];
-
-
-console.log(dot1.dataset.img);
-console.log(dot2.dataset.img);
-console.log(dot3.dataset.img);
-console.log(carsouelImage.src);
-
-console.log(images)
-
-// function changeImageSrc1(){
-//     carsouelImage.src = images[0];
-// }
-// function changeImageSrc2(){
-//     carsouelImage.src = images[1];
-// }
-// function changeImageSrc3(){
-//     carsouelImage.src = images[2];
-// }
 
 dot1.onclick = EventHandler;
 dot2.onclick = EventHandler;
